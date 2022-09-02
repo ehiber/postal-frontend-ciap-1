d743336b6b5992a2404062bb54a4ae327c946648
ehiber <ehiber5@hotmail.com>
2022-09-02 22:54:08 +0000

    ejercicio para los alumnos! mas dedicados del ciap :smile:

--- src/app.js
+++ src/app.js
@@ -5,7 +5,54 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const randomTitle = () => {
+  let titles = [
+    "Estoy pensando en programar",
+    "Este fin de semana entrego los proyectos",
+    "Que divertido ir a clases el viernes",
+    "Me canse"
+  ];
+  return titles[Math.floor(Math.random() * titles.length)];
+};
+
+const changeTitle = h1 => {
+  let newTitle = randomTitle();
+  while (newTitle == h1.innerHTML) {
+    newTitle = randomTitle();
+  }
+  return newTitle;
+};
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let titles = document.querySelectorAll(".title");
+  let h1;
+  let randomTitleText = randomTitle();
+  for (let title of titles) {
+    h1 = document.createElement("h1");
+    h1.innerHTML = randomTitleText;
+    title.innerHTML = "";
+    title.appendChild(h1);
+  }
+
+  let footer = document.getElementById("footer");
+  footer.innerHTML = "";
+  let button = document.createElement("button");
+  button.innerHTML = "Cambia de título";
+  footer.appendChild(button);
+
+  // let interval = setInterval(() => {
+  //   let newTitle = changeTitle(h1);
+  //   h1.innerHTML = newTitle;
+  // }, 1000);
+
+  // button.addEventListener("click", () => {
+  //   let newTitle = changeTitle(h1);
+  //   h1.innerHTML = newTitle;
+  //   clearInterval(interval);
+  //   interval = setInterval(() => {
+  //     let newTitle = changeTitle(h1);
+  //     h1.innerHTML = newTitle;
+  //   }, 1000);
+  // });
 };
